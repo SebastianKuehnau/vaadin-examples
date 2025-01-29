41dcb135045553b6b6e61d7cbac20d1f0a163b8e
start.vaadin.com <no-reply@vaadin.com>
2025-01-29 07:56:47 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/pagetitle/PagetitleView.java
@@ -0,0 +1,35 @@
+package com.example.application.views.pagetitle;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Pagetitle")
+@Route("pagetitle")
+@Menu(order = 1, icon = LineAwesomeIconUrl.NEWSPAPER)
+public class PagetitleView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public PagetitleView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
